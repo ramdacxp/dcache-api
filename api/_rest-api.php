f28340f27c28866aea9581bbad8890ad3c73f07a
Michael Hoser <michael.hoser@outlook.com>
2024-10-05 22:20:39 +0200

    settings

--- api/_rest-api.php
+++ api/_rest-api.php
@@ -10,14 +10,15 @@ class RestApi
   const CODE_404_NOT_FOUND = 404;
   const CODE_500_INTERNAL_SERVER_ERROR = 500;
 
-  public static function respond($code, $data)
+  public static function respond($code, $data, $exit = true)
   {
     header('Content-Type: application/json');
     http_response_code($code);
     echo json_encode($data);
+    if ($exit) exit();
   }
 
-  public static function respondError($code, $message)
+  public static function respondError($code, $message, $exit = true)
   {
     $json = [
       "kind"  => "error",
@@ -25,5 +26,47 @@ class RestApi
       "message" => $message
     ];
     RestApi::respond($code, $json);
+    if ($exit) exit();
+  }
+
+  public static function getRequestUri()
+  {
+    $request = "";
+
+    if (is_string($_SERVER["REQUEST_URI"])) {
+      $request = $_SERVER["REQUEST_URI"];
+    }
+
+    if (!str_ends_with($request, "/")) {
+      $request .= "/";
+    }
+
+    return $request;
+  }
+
+  public static function getRequestPart($idx, $lowerCase = true)
+  {
+    $result = "";
+
+    $segments = explode("/", RestApi::getRequestUri());
+    if ($idx >= 0 && $idx < count($segments)) {
+      $result = $segments[$idx];
+    }
+
+    if ($lowerCase) {
+      $result = strtolower($result);
+    }
+
+    return $result;
+  }
+
+  public static function isGet()
+  {
+    return $_SERVER["REQUEST_METHOD"] == "GET";
+  }
+
+  public static function isPost()
+  {
+    return $_SERVER["REQUEST_METHOD"] == "POST";
   }
 }
--- api/index.php
+++ api/index.php
@@ -1,14 +1,43 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . '_rest-api.php';
 
-$request = "";
-if (is_string($_SERVER['REQUEST_URI'])) {
-  $request = $_SERVER['REQUEST_URI'];
+// phpinfo( INFO_VARIABLES);
+
+// INIT
+// =============================================================================
+$configFilename = __DIR__ . DIRECTORY_SEPARATOR . 'settings.php';
+$isConfigured = file_exists($configFilename);
+$request = RestApi::getRequestUri();
+if ($request === "/") {
+  RestApi::respondError(RestApi::CODE_400_BAD_REQUEST, "No api target selected.");
 }
 
+// CONFIG
+// =============================================================================
+if (RestApi::getRequestPart(1) == "config") {
+  if (RestApi::isGet()) {
+    RestApi::respond(RestApi::CODE_200_OK, ["isConfigured" => $isConfigured]);
+  } else if (RestApi::isPost()) {
+    if ($isConfigured) {
+      RestApi::respondError(RestApi::CODE_403_FORBIDDEN, "API is already configured. Delete setting file from server and try again.");
+    } else {
+      $data = json_decode(file_get_contents("php://input"), true);
+      if ($data == null) {
+        RestApi::respondError(RestApi::CODE_400_BAD_REQUEST, "Request body contains no settings.");
+      }
+      $settings = "<" . "?php\n\$settings=[];\n";
+      foreach ($data as $key => $value) {
+        $settings .=  "\$settings[\"" . $key . "\"]=" . json_encode($value) . ";\n";
+      }
+      file_put_contents($configFilename, $settings);
+      RestApi::respond(RestApi::CODE_200_OK, $data);
+    }
+  }
+}
+
+// NOT IMPLEMENTED
+// =============================================================================
 RestApi::respondError(
   RestApi::CODE_500_INTERNAL_SERVER_ERROR,
   "Request '" . $request . "' is not implemented"
 );
-
-// phpinfo( INFO_VARIABLES);
