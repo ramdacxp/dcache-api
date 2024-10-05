97f207ddd7da3ec3eb7278ee8a090717f8581713
Michael Hoser <michael.hoser@outlook.com>
2024-10-05 21:32:15 +0200

    Rewrite all api sub urls

--- api/index.php
+++ api/index.php
@@ -1,5 +1,14 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . '_rest-api.php';
 
+$request = "";
+if (is_string($_SERVER['REQUEST_URI'])) {
+  $request = $_SERVER['REQUEST_URI'];
+}
 
-RestApi::respondError(RestApi::CODE_500_INTERNAL_SERVER_ERROR, 'Not implemented');
+RestApi::respondError(
+  RestApi::CODE_500_INTERNAL_SERVER_ERROR,
+  "Request '" . $request . "' is not implemented"
+);
+
+// phpinfo( INFO_VARIABLES);
